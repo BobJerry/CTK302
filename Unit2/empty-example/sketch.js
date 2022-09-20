c2a59d9aefa03bc3b984436e3860ae9d6d826ae8
BobJerry <btmurp3@ilstu.edu>
2022-09-20 11:34:23 -0500

    j

--- Unit2/empty-example/sketch.js
+++ Unit2/empty-example/sketch.js
@@ -1,13 +1,20 @@
 let x = 0;
+let f1;
 function setup() {
   createCanvas(500, 500);
+  f1 = loadFont("assets/mugs.ttf");
 }
 
 function draw() {
   background(100);
-text("Unit 2 Rules", x,100,100,100);
+  textSize(100);
+  textFont(f1);
+text("Unit 2 Rules", x,200);
 x +=5;
 if (x>width) {
-  x=0;
+  x=100;
 }
+}
+function avatar() {
+  
 }
