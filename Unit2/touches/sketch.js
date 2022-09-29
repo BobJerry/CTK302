474b61a22c1bc7074ae83d8d86da4a6a7fb6b79a
BobJerry <btmurp3@ilstu.edu>
2022-09-29 12:09:14 -0500

    j

--- Unit2/touches/sketch.js
+++ Unit2/touches/sketch.js
@@ -17,16 +17,19 @@ function draw() {
       case 1: 
        text("beep", 5, 22) ; 
       // put a picture here
+      Image(assets/saul.jpg)
       break ;
       
       case 2:
       text("boop", 5, 22) ; 
-            // put a picture here
+      // put a picture here
+      Image(assets/finger.jpg)
       break ;
       
       case 3:
      text("beep", 5, 22) ; 
-            // put a picture here
+      // put a picture here
+      Image(assets/waltuh.jpg)
       break ;
     
       
