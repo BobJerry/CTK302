53f4fd7b0f568299694e7a22a00360d05653c5ba
BobJerry <btmurp3@ilstu.edu>
2022-10-04 11:35:23 -0500

    j

--- Unit2/Spotlight/sketch.js
+++ Unit2/Spotlight/sketch.js
@@ -12,22 +12,45 @@ function draw() {
   rect(width/2, height/2, 200, 600);
   switch (state) {
 
-    case 0:
+    case 0: //green
+      fill("grey")
+      ellipse(width/2, height/2-170, 150, 150)
       ellipse(width/2, height/2, 150, 150)
-      ellipse(width/2, height/2, 150, 150)
-      ellipse(width/2, height/2, 150, 150)
+      fill("green")
+      ellipse(width/2, height/2+170, 150, 150)
+      v=10;
       break;
 
-    case 1:
-      text("1", 100, 100);
-      break;
+    case 1: //yellow
+    fill("grey")
+    ellipse(width/2, height/2-170, 150, 150)
+    fill("yellow")
+    ellipse(width/2, height/2, 150, 150)
+    fill("grey")
+     ellipse(width/2, height/2+170, 150, 150)
+     v=5;
+    break;
 
-    case 2:
-      text("2", 100, 100);
-      break;
-
+    case 2: //red
+    fill("red")
+    ellipse(width/2, height/2-170, 150, 150)
+    fill("grey")
+    ellipse(width/2, height/2, 150, 150)
+    ellipse(width/2, height/2+170, 150, 150)
+    v=0;
+    break;
   }
 }
+
+timer++;
+if (timer > 3*60) {
+  timer = 0;
+  state++;
+  if (state>2) state=0;
+
+}
+fill("blue")
+rect(x, height - 75, 100, 50);
 
 function mouseReleased() {
   state++;
