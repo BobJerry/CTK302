7de9f365ac12759475d7c7c14b2a401417c33bd4
BobJerry <btmurp3@ilstu.edu>
2022-10-08 16:57:53 -0500

    j

--- Unit2/Mp2/sketch.js
+++ Unit2/Mp2/sketch.js
@@ -16,35 +16,33 @@ function draw() {
   background("grey");
   imageMode(CENTER);
   image(i1, width/2, height/2, width, height) ;
-  image(i2, width/2, height/2+210, 150, 200) ;
-  image(i3, width/2, height/2-175, 225, 225) ;
-
-
 fill("white");
-text("You have 10 seconds to decide the fate of the egg", 190, 50);
 textSize(20);
 switch (state) {
 
   case 0: //normal state
+  text("You have 10 seconds to decide the fate of the egg", 190, 50);
   image(i3, width/2, height/2-175, 225, 225) ;
   image(i2, width/2, height/2+210, 150, 200) ;
     v=10;
     break;
 
   case 1: //sawblade half way down
-  image(i3, width/2, height/2-175, 225, 225) ;
+  text("You have 10 seconds to decide the fate of the egg", 190, 50);
+  image(i3, width/2, height/2-25, 225, 225) ;
   image(i2, width/2, height/2+210, 150, 200) ;
    v=5;
   break;
 
   case 2: //sawblade replaces egg
-  image(i3, width/2, height/2-175, 225, 225) ;
+  text("The egg cracked under pressure :(")
+  image(i3, width/2, height/2+210, 225, 225) ;
   v=0;
   break;
 }
 
 timer++;
-if (timer > 7 * 60) {
+if (timer > 5 * 60) {
 timer = 0;
 state++;
 if (state > 2) state=0;
