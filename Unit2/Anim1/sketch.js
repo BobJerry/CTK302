6693a1a1b298fda002c79cd985c0ab506796ea6f
BobJerry <btmurp3@ilstu.edu>
2022-11-01 16:29:55 -0500

    j

--- Unit2/Anim1/sketch.js
+++ Unit2/Anim1/sketch.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let x = 0;
 let f1;
 function setup() {
@@ -24,8 +23,6 @@ function avatar() {
      fill("black");
     ellipse (315,350,15,15);
     ellipse(365, 350, 15, 15);
-=======
-<<<<<<< HEAD
 let x = 0;
 let f1;
 function setup() {
@@ -51,32 +48,4 @@ function avatar() {
      fill("black");
     ellipse (315,350,15,15);
     ellipse(365, 350, 15, 15);
-=======
-let x = 0;
-let f1;
-function setup() {
-  createCanvas(500, 500);
-  f1 = loadFont("assets/mugs.ttf");
-}
-
-function draw() {
-  background(100);
-  textSize(100);
-  textFont(f1);
-text("Unit 2 Rules", x,200);
-x +=5;
-if (x>width) {
-  x=100;
-}
-}
-function avatar() {
-  fill("white");
-    ellipse(300,300, 100, 200)
-	rect(300, 400, 100, 100);
-    rectMode(BOTTOM);
-     fill("black");
-    ellipse (315,350,15,15);
-    ellipse(365, 350, 15, 15);
->>>>>>> fd0a33306c5c9cfc1ff32bc84550c5f1d772536b
->>>>>>> 1d505c5efa9170b9e4ab79996716a890b368fef1
 }
