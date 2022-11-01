2f3c9c444bbf27dc7aa19a9dac6b31c493253f18
BobJerry <btmurp3@ilstu.edu>
2022-11-01 16:55:33 -0500

    j

--- Unit2/Anim1/sketch.js
+++ Unit2/Anim1/sketch.js
@@ -23,29 +23,4 @@ function avatar() {
      fill("black");
     ellipse (315,350,15,15);
     ellipse(365, 350, 15, 15);
-let x = 0;
-let f1;
-function setup() {
-  createCanvas(500, 500);
-  f1 = loadFont("assets/mugs.ttf");
-}
-
-function draw() {
-  background(100);
-  textSize(100);
-  textFont(f1);
-text("Unit 2 Rules", x,200);
-x +=5;
-if (x>width) {
-  x=100;
-}
-}
-function avatar() {
-  fill("white");
-    ellipse(300,300, 100, 200)
-	rect(300, 400, 100, 100);
-    rectMode(BOTTOM);
-     fill("black");
-    ellipse (315,350,15,15);
-    ellipse(365, 350, 15, 15);
 }
--- Unit2/Anim2/sketch.js
+++ Unit2/Anim2/sketch.js
@@ -25,31 +25,4 @@ function avatar() {
      fill("black");
     ellipse (320,255,15,15);
     ellipse(370, 255, 15, 15);
-let x = 0;
-function setup() {
-  createCanvas(500, 500);
-  noStroke();
-}
-
-function draw() {
-  background(100);
-  push();
-  translate(x,0);
- avatar();
- x += 3;
- if (x>width) {
-  x= 0;
-}
-pop();
-}
-
-
-function avatar() {
-  fill("white");
-  ellipse(350,300, 100, 200)
-	rect(300, 300, 100, 150);
-  //rectMode(BOTTOM);
-     fill("black");
-    ellipse (320,255,15,15);
-    ellipse(370, 255, 15, 15);
 }
