8f30d51b5c8d2e3dc6f8169d19cd96bb9b2a1b2a
BobJerry <btmurp3@ilstu.edu>
2022-11-10 12:18:11 -0600

    j

--- Extra/sketch.js
+++ Extra/sketch.js
@@ -1,7 +1,9 @@
 let f1;
 let i1;
+let state = 0;
+let timer = 0;
 function setup() {
-  createCanvas(1000, 570);
+  createCanvas(1000, 600);
   saul = loadImage("Assets/saul.jpg");
   vcr = loadFont("Assets/VCR.ttf");
 }
@@ -10,10 +12,15 @@ function draw() {
 background("blue");
 fill("white");
 textFont(vcr, 60);
-text("Better Call Saul", 200, 300);
+text("BETTER CALL SAUL", 200, 300);
 textFont(vcr, 30);
-text("Created by", 350, 400);
-text("Vince Gilligan", 350, 500);
-text("And Peter Gould", 350, 600);
+text("CREATED BY", 400, 375);
+text("VINCE GILLIGAN", 360, 405);
+text("&",475, 435)
+text("PETER GOULD", 390, 460);
 text("Rec", 45, 45);
+switch (state) {
+  case 
+
+}
 }
